fe39e3f4a720730aa11d65b37bbd13f1f7a847f7
gameleonpptgames <98007819+gameleonpptgames@users.noreply.github.com>
2023-03-02 12:35:16 +0900

    Update finalwebpage.js

--- finalwebpage.js
+++ finalwebpage.js
@@ -18,7 +18,7 @@ menu.addEventListener("click", () => {
       slidesPerView: 'auto',
       coverflowEffect: {
         rotate: 20,
-        stretch: 0,
+        stretch: 2,
         depth: 200,
         modifier: 1,
         slideShadows: true,
