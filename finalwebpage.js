d869230d61ce223a30e83ee0c9a45252621aa487
gameleonpptgames <98007819+gameleonpptgames@users.noreply.github.com>
2023-03-02 13:14:45 +0900

    Update finalwebpage.js

--- finalwebpage.js
+++ finalwebpage.js
@@ -12,14 +12,14 @@ menu.addEventListener("click", () => {
 //3D Carousel//
 
     var swiper = new Swiper('.swiper-container', {
-      effect: 'coverflow',
+//       effect: 'coverflow',
       grabCursor: true,
       centeredSlides: true,
       slidesPerView: 'auto',
       coverflowEffect: {
         rotate: 20,
         stretch: 0,
-        depth: 500,
+        depth: 200,
         modifier: 1,
         slideShadows: true,
       },
