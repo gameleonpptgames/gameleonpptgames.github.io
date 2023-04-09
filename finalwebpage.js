7ce9ceb34c1e9b3618b6c225b2c5a99dec98cc38
gameleonpptgames <98007819+gameleonpptgames@users.noreply.github.com>
2023-04-09 18:56:46 +0900

    Update finalwebpage.js

--- finalwebpage.js
+++ finalwebpage.js
@@ -1,32 +1,49 @@
 // Navbar
-const menu = document.querySelector(".menu");
-const navbar = document.querySelector(".navbar");
+// const menu = document.querySelector(".menu");
+// const navbar = document.querySelector(".navbar");
 
-menu.addEventListener("click", () => {
-  navbar.classList.toggle("change");
-  menu.classList.toggle('change')
-});
+// menu.addEventListener("click", () => {
+//   navbar.classList.toggle("change");
+//   menu.classList.toggle('change')
+// });
 // End of Navbar
 
 
 //3D Carousel//
 
-    var swiper = new Swiper('.swiper-container', {
-      effect: 'coverflow',
-      grabCursor: true,
+//     var swiper = new Swiper('.swiper-container', {
+//       effect: 'coverflow',
+//       grabCursor: true,
+//       centeredSlides: true,
+//       slidesPerView: 'auto',
+//       coverflowEffect: {
+//         rotate: 20,
+//         stretch: 0,
+//         depth: 200,
+//         modifier: 1,
+//         slideShadows: true,
+//       },
+//       loop: true,
+//       autoplay: {
+//         delay: 900,
+//         disableOnInteraction: false,
+//       },
+//     });
+
+  var swiper = new Swiper(".mySwiper", {
+      spaceBetween: 30,
       centeredSlides: true,
-      slidesPerView: 'auto',
-      coverflowEffect: {
-        rotate: 20,
-        stretch: 0,
-        depth: 200,
-        modifier: 1,
-        slideShadows: true,
-      },
-      loop: true,
       autoplay: {
-        delay: 900,
+        delay: 2500,
         disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
       },
     });
 
